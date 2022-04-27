2f771114a3af9804ac07801d2da2b08355ac3bf3
anmolsekhon590 <anmolsekhon590@gmail.com>
2022-04-27 17:24:10 -0400

    added comments

--- src/main/java/com/anmolsekhon/interviewproject/domain/Asset.java
+++ src/main/java/com/anmolsekhon/interviewproject/domain/Asset.java
@@ -2,7 +2,6 @@ package com.anmolsekhon.interviewproject.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Generated;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
@@ -13,11 +12,15 @@ import java.time.LocalDate;
 
 import static javax.persistence.GenerationType.AUTO;
 
+/**
+ * This Asset Class represents an Entity 'Asset' in the Database
+ * */
 @Data
 @Entity
 @AllArgsConstructor
 @RequiredArgsConstructor
 public class Asset {
+//    Defining assetId as primary key and its generation is set to auto
     @Id
     @GeneratedValue(strategy = AUTO)
     private Long assetId;
