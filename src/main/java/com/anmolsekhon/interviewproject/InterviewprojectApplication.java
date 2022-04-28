f0f5675eac3140c4c6dc5307fe6da15a3f9c3f2c
anmolsekhon590 <anmolsekhon590@gmail.com>
2022-04-28 12:34:08 -0400

    Added more dummy data

--- src/main/java/com/anmolsekhon/interviewproject/InterviewprojectApplication.java
+++ src/main/java/com/anmolsekhon/interviewproject/InterviewprojectApplication.java
@@ -17,25 +17,35 @@ import java.util.ArrayList;
 @SpringBootApplication
 public class InterviewprojectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InterviewprojectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewprojectApplication.class, args);
+    }
 
-	@Bean
-	CommandLineRunner commandLineRunner(AssetService assetService) {
-		return args->{
+    @Bean
+    CommandLineRunner commandLineRunner(AssetService assetService) {
+        return args -> {
 //			Inserting Sample Data
-			assetService.saveAsset(
-					new Asset(1L, "Dell Optiplex", "A Desktop PC",
-							220.00, LocalDate.of(2020, Month.AUGUST, 24), 1L)
-			);
+            assetService.saveAsset(
+                    new Asset(1L, "Dell Optiplex", "A Desktop PC",
+                            220.00, LocalDate.of(2020, Month.AUGUST, 24), 1L)
+            );
 
-		};
-	}
+            assetService.saveAsset(
+                    new Asset(2L, "Windows 10 - Home", "An Operating System",
+                            80.0, LocalDate.of(2021, Month.FEBRUARY, 19), 0L)
+            );
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-	}
+            assetService.saveAsset(new Asset(3L, "HP Server", "Server Hardware",
+                    500.50, LocalDate.of(2018, Month.JULY, 2), 1L)
+
+            );
+
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 
 }
